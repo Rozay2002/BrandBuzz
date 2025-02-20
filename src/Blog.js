8984602129f33102b48ae00030dbb8bf2fcd371d
Robert <robertassaw2002@gmail.com>
2025-02-20 02:58:20 -0800

    changes made

--- src/Blog.js
+++ src/Blog.js
@@ -13,13 +13,13 @@ const Blog = () => {
                     <p>Contact us to boost your undersanding</p>
                     <h2>2</h2>
                     <h4>Consult</h4>
-                    <p>Contact us to boost your undersanding</p>
+                    <p>Consult us to boost your undersanding</p>
                     <h2>3</h2>
                     <h4>Place order</h4>
-                    <p>Contact us to boost your undersanding</p>
+                    <p>You can place your order</p>
                     <h2>4</h2>
                     <h4>Payment</h4>
-                    <p>Contact us to boost your undersanding</p>
+                    <p>Make payment of your ordered item</p>
                 </div>
                 <div className="buttons">
                     <button className="get">Get Started</button>
